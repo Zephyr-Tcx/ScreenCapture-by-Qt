89e21afc4d7144c6d0ecf6e20d209089369d1b27
Zephyr_T <Zephyr_T@outlook.com>
2026-02-08 15:18:01 +0800

    CMake构建

---
+++ build/CMakeFiles/ScreenCaptureTool_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ScreenCaptureTool_autogen"
+  "ScreenCaptureTool_autogen/include/ui_mainwindow.h"
+  "ScreenCaptureTool_autogen/mocs_compilation.cpp"
+  "ScreenCaptureTool_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ScreenCaptureTool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
