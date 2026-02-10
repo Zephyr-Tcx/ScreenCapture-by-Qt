9966b3cdeaf3a6ad1e1c94b8ef765286217f87d0
Zephyr_T <Zephyr_T@outlook.com>
2026-02-10 10:17:13 +0800

    1

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ScreenCaptureTool_FILE D:/ScreenCaptureTool/build/ScreenCaptureTool.exe)
-set(__QT_DEPLOY_TARGET_ScreenCaptureTool_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ScreenCaptureTool_RUNTIME_DLLS D:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
